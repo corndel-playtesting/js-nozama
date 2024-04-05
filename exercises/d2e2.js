2d6b49e1118faa38cb36b67eb76325d824fb14f4
Simon Haines <nimon@hotmail.co.uk>
2024-04-05 16:32:13 +0100

    solve d2e2

--- exercises/d2e2.js
+++ exercises/d2e2.js
@@ -1,22 +1,22 @@
 import express from 'express'
 const app = express()
 
-// Hint: res.send() will not accept numbers - you will need to convert your
-// result to a string before sending it
-
 app.get('/sumup', (req, res) => {
-  // TODO
-  // Responds with the sum of all numbers from 1 to n
-  // If no param is provided, respond with the number 0
-  // e.g.: GET /sumup?n=5 should respond with '15'
-  // e.g.: GET /sumup should respond with '0'
+  const n = req.query.n
+  if (n) {
+    let sum = 0
+    for (let i = 1; i <= n; i++) {
+      sum += i
+    }
+    res.send(sum.toString())
+  } else {
+    res.send('0')
+  }
 })
 
 app.get('/multiply/:x/:y', (req, res) => {
-  // TODO
-  // Responds with the product of x and y
-  // e.g.: GET /multiply/5/4 should respond with '20'
+  const { x, y } = req.params
+  res.send((x * y).toString())
 })
 
-// don't change below:
 export default app

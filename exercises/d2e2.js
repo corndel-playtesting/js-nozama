f8d1b128fd435ae6fb2f1faa4cf479cc1c410684
Andy Huang <cerberus@ayxh.dev>
2024-04-12 15:47:10 +0000

    solve d2e2

--- exercises/d2e2.js
+++ exercises/d2e2.js
@@ -15,7 +15,12 @@ app.get('/sumup', (req, res) => {
    * res.send() the sum of integers from 1 to n
    * if n is not given, respond with 0
    * e.g. /sumup?n=4 => 10
+
    */
+  const n = Number.parseInt(req.query.n ?? 0)
+  const count = (n * (n + 1)) / 2
+
+  res.send(count.toString())
 })
 
 app.get('/multiply/:x/:y', (req, res) => {
@@ -23,6 +28,9 @@ app.get('/multiply/:x/:y', (req, res) => {
    * This endpoint responds with the product of x and y
    * e.g. /multiply/:3/:5 => 15
    */
+  const x = Number.parseInt(req.params.x)
+  const y = Number.parseInt(req.params.y)
+  res.send((x * y).toString())
 })
 
 export default app

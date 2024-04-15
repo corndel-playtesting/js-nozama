99307dd72d2546032ccb887cc54b1929e90814a5
Andy Huang <cerberus@ayxh.dev>
2024-04-15 12:56:02 +0000

    solve d3e2

--- exercises/d3e2.js
+++ exercises/d3e2.js
@@ -14,8 +14,15 @@ export class AppError extends Error {
 export class Account {
   static updateUsername(newUsername, password) {
     // If newUsername is not given, throw an AppError with code 400 (Bad Request)
+    if (!newUsername) throw new AppError('No username given', 400)
+
     // If password is not given, throw an AppError with code 401 (Unauthorized)
+    if (!password) throw new AppError('No password given', 401)
+
     // If password is given but not correct, throw an AppError with code 403 (Forbidden)
+    if (password !== account.password) throw new AppError('Incorrect password', 403)
+
     // If newUsername is given and password is correct, update the username and return the new username
+    account.username = newUsername
   }
 }
